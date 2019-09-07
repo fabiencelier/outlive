7df706e9c02f8dd1b6fcbaa06734352d8e98c4aa
fabiencelier <fabien.celier@polytechnique.edu>
2019-09-07 20:16:16 +0200

    Display statistics with my age

--- javascript/src/components/Statistics.js
+++ javascript/src/components/Statistics.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Statistic } from 'antd';
 import { addTabs } from './Tabular';
+import { diffWithTodayInDays } from '../date/date';
 
+class StatisticsContent extends Component {
 
-const StatisticsContent = (props) => (
-  <div>
-    <h1 style={{color: "white"}}>Statistics</h1>
-    <h3 style={{color: "white"}}>Survival Time</h3>
-    <Statistic title="Days" value={31893} />
-    <Statistic title="Hours" value={31893*24} />
-    <Statistic title="Minutes" value={31893*24*60} />
-    <Statistic title="Seconds" value={31893*24*60*60} />
-  </div>
-)
+  constructor(props) {
+    super(props);
+    // Don't call this.setState() here!
+    this.state = { age : diffWithTodayInDays(new Date("1993-10-12")) };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 style={{color: "white"}}>Statistics</h1>
+        <h3 style={{color: "white"}}>Survival Time</h3>
+        <Statistic title="Days" value={this.state.age} />
+        <Statistic title="Hours" value={this.state.age*24} />
+        <Statistic title="Minutes" value={this.state.age*24*60} />
+        <Statistic title="Seconds" value={this.state.age*24*60*60} />
+      </div>
+    )
+  }
+}
 
 export const Statistics = addTabs(StatisticsContent, "statistics");
