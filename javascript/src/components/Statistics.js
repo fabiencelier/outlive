bd39157433e7d274cb9d97feea580d159185dec0
fabiencelier <fabien.celier@polytechnique.edu>
2019-09-07 19:34:24 +0200

    Remove duplicate statistics

--- javascript/src/components/Statistics.js
+++ javascript/src/components/Statistics.js
@@ -5,19 +5,8 @@ import { addTabs } from './Tabular';
 
 const StatisticsContent = (props) => (
   <div>
-    <h1>Statistics</h1>
-    <h3>Survival Time</h3>
-    <p>Units below day are approximative</p>
-    <Statistic title="Days" value={31893} />
-    <Statistic title="Hours" value={31893*24} />
-    <Statistic title="Minutes" value={31893*24*60} />
-    <Statistic title="Seconds" value={31893*24*60*60} />
-    <p>Units below day are approximative</p>
-    <Statistic title="Days" value={31893} />
-    <Statistic title="Hours" value={31893*24} />
-    <Statistic title="Minutes" value={31893*24*60} />
-    <Statistic title="Seconds" value={31893*24*60*60} />
-    <p>Units below day are approximative</p>
+    <h1 style={{color: "white"}}>Statistics</h1>
+    <h3 style={{color: "white"}}>Survival Time</h3>
     <Statistic title="Days" value={31893} />
     <Statistic title="Hours" value={31893*24} />
     <Statistic title="Minutes" value={31893*24*60} />
