3b591ca5adb013d662d7fa314ce3f7923fdd5867
fabiencelier <fabien.celier@polytechnique.edu>
2019-09-07 21:25:04 +0200

    Theme in description is similar to global dark theme

--- javascript/src/components/Description.js
+++ javascript/src/components/Description.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
-import { Avatar, PageHeader } from 'antd';
+import { Avatar, Icon, PageHeader, Skeleton } from 'antd';
 import { getFromId } from '../api/query';
 
+
+function pad(num) {
+  var s = "00" + num;
+  return s.substr(s.length - 2);
+}
+
+const formatDate = (date) => {
+  return `${date.getFullYear()}-${pad(date.getMonth()+1)}-${pad(date.getDate())}`
+}
 
 export class Description extends Component{
 
@@ -16,18 +25,29 @@ export class Description extends Component{
 
   render() {
     return (
-      <div>
-        {this.state.description &&
-        <div>
-          <PageHeader
-            title={this.state.description.title}
-            subTitle={`${this.state.description.days} days`}
-            onBack={() => this.props.history.goBack()}
-            />
-          <Avatar size={128} src={this.state.description.image} />
-
-        </div>}
-        <p>{console.log(this.props)}</p>
+      <div style={{minHeight: "100vh", backgroundColor:"#282c34"}}>
+        <PageHeader
+          style={{
+            backgroundColor: "black",
+            color: "white",
+            paddingTop: "1em",
+            paddingBottom: "1em",
+          }}
+          title={this.state.description && <div style={{color: "white"}}>{this.state.description.title}</div>}
+          subTitle={this.state.description && <div style={{color: "white"}}>{`${this.state.description.days} days`}</div>}
+          onBack={() => this.props.history.goBack()}
+          backIcon={<Icon style={{color: "white"}} type="arrow-left" />}
+          />
+          {
+            this.state.description
+            ? <div style={{backgroundColor:"#282c34", color:"white"}}>
+              <Avatar size={128} src={this.state.description.image} />
+              <p>Birth : {formatDate(this.state.description.birthDate)}</p>
+              <p>Death : {formatDate(this.state.description.deathDate)}</p>
+              {this.state.description.description && <p>{this.state.description.description}</p>}
+            </div>
+            : <Skeleton/>
+          }
       </div>
     )
   }
