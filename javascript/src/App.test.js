8de0da1a4e3536c904f28ac30e34c2d5703cdd02
fabiencelier <fabien.celier@polytechnique.edu>
2019-09-08 02:03:10 +0200

    Fix App test

--- javascript/src/App.test.js
+++ javascript/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const store = configureStore();
+
+  const ConnectedApp = (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+  ReactDOM.render(ConnectedApp, div);
   ReactDOM.unmountComponentAtNode(div);
 });
